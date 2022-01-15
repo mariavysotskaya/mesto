498813c63ba96ce6af3d9c8bf3f84b53ab5daf48
mariavysotskaya <vysotskaya92@gmail.com>
2022-01-15 19:09:51 +0300

    errors fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,4 @@
-let likeBtn = document.querySelectorAll('.element__like-btn');
-
-for (let i=0; i<likeBtn.length; i++){
-    if (likeBtn[i].addEventListener('click', function(){
-        likeBtn[i].classList.toggle('element__like-btn_active');
-    }));
-};
+let likeBtn = document.querySelectorAll('.card__like-btn');
 
 const editBtn = document.querySelector('.profile__edit-btn');
 const popup = document.querySelector('.popup');
@@ -12,49 +6,43 @@ const formElement = document.querySelector('.popup__form');
 const saveBtn = formElement.querySelector('.popup__save-btn');
 const closeBtn = document.querySelector('.popup__close-btn');
 
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
 let nameInput = formElement.querySelector('.popup__input-fld_type_name');
 let jobInput = formElement.querySelector('.popup__input-fld_type_job');
 
 function openPopup(){
     popup.classList.add('popup_open');
-    document.body.style.overflow = 'hidden';
-    nameInput.value = document.querySelector('.profile__name').textContent;
-    jobInput.value = document.querySelector('.profile__job').textContent;
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
 }
 
 function closePopup(){
     popup.classList.remove('popup_open');
-    document.body.style.overflow = '';
 }
-
-if (editBtn.addEventListener('click', function(){
-    openPopup();
-}));
-
-if (closeBtn.addEventListener('click',function(){
-    closePopup();
-}));
-
-if (saveBtn.addEventListener('click', function(){
-    closePopup();
-}));
-
-if (popup.addEventListener('click',function(event){
-    if (event.target === popup) {
-    closePopup();
-  }
-}));
-
-if (document.addEventListener('keydown', function(event){
-    if (event.code == 'Escape') {
-        closePopup();
-    }
-}));
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    document.querySelector('.profile__name').textContent = nameInput.value;
-    document.querySelector('.profile__job').textContent = jobInput.value;
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
 }
+
+editBtn.addEventListener('click', openPopup);
+
+closeBtn.addEventListener('click', closePopup);
+
+saveBtn.addEventListener('click', closePopup);
+
+popup.addEventListener('click',function(event){
+    if (event.target === popup) {
+        closePopup();
+  }
+});
+
+document.addEventListener('keydown', function(event){
+    if (event.code == 'Escape') {
+        closePopup();
+    }
+});
 
 formElement.addEventListener('submit', formSubmitHandler);
